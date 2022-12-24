55e4be7bed49348a58af85bc22ccf7540758db41
wilberth <wgcampos15@gmail.com>
2022-12-24 01:35:18 -0600

    first commit

---
+++ models/server.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+
+class Server {
+    private app: express.Application;
+    private port:string;
+    constructor() {
+        this.app=express();
+        this.port = process.env.PORT || '3000';
+
+        
+    }
+
+    listen(){
+        this.app.listen(this.port,()=>{
+            console.log("servidor arriba")
+        })
+    }
+}
+
+export default Server;
